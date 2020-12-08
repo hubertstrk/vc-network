b8aa8d92afb19b1c88f0fa81c03ec44f87e9aa58
Hubert Stärk <hubert.staerk@gmail.com>
2020-12-08 22:45:17 +0100

    :tada: output json structure for networks

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs-extra')
 const path = require('path')
 const compiler = require('vue-template-compiler')
-let acorn = require("acorn")
+let acorn = require('acorn')
+const {flatten} = require('lodash')
 
-const rootPath = 'C:\\Sources\\notes2.bootstrap\\'
+const rootPath = 'C:\\Sources\\novelc19'
 
 /**
  * Returns all file with extension .vue
@@ -36,20 +37,57 @@ const getVueFiles = (dirPath, arrayOfFiles) => {
  */
 const readFile = (path) => fs.readFileSync(path, 'utf8')
 
-const components = getVueFiles(rootPath).map((path) => {
+const components = getVueFiles(rootPath).reduce((acc, path) => {
   const sfc = readFile(path)
   const compiled = compiler.parseComponent(sfc)
-  return {path, content: compiled.script.content}
-})
-
-
-const imports = components.reduce((acc, curr) => {
-  const js = acorn.parse(curr.content.toString(), {ecmaVersion: 2020, sourceType: 'module'})
-  const parsedImports = js.body.filter(x => x.type === 'ImportDeclaration').map(x => ({name: x.specifiers[0].local.name, value: x.source.value}))
-  if (parsedImports.length > 0) {
-    acc[curr.path] = parsedImports
+  if (compiled.script) {
+    acc.push({path, content: compiled.script.content})
   }
   return acc
-}, {})
+}, [])
 
-console.info({imports})
+
+const sfcImports = components.reduce((acc, curr) => {
+  const parsed = acorn.parse(curr.content.toString(), {ecmaVersion: 2020, sourceType: 'module'})
+  const is = parsed.body
+    .filter(x => x.type === 'ImportDeclaration')
+    .reduce((acc, curr) => {
+      if (curr.specifiers && curr.specifiers.length > 0) {
+        acc.push(curr.specifiers[0].local.name)
+      }
+      return acc
+    },[])
+
+  if (is.length > 0) {
+    //acc[curr.path] = parsedImports
+    const source = path.parse(curr.path).name
+    acc.push({source, is})
+  }
+  return acc
+}, [])
+
+let links = sfcImports.map(sfcImport => {
+  return sfcImport.is.map(x => ({source: sfcImport.source, target: x}))
+})
+links = flatten(links)
+
+let nodes = sfcImports.map(sfcImport => {
+  return [sfcImport.source, ...sfcImport.is]
+})
+nodes = flatten(nodes)
+nodes = new Set(nodes)
+
+const network = {
+  nodes: [...nodes].map(node => ({id: node, name: node})),
+  links: links
+}
+
+// { "nodes": [
+//   { "id": 1, "name": "A" },
+//   { "id": 2, "name": "B" }
+// ],
+// "links": [
+//   { "source": 1, "target": 2 }
+// ]}
+
+console.info(JSON.stringify(network))
